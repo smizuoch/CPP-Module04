1069151477980de8a1e8df17379b570107dba675
smizuoch <shotaro.mizuochi@proton.me>
2024-08-11 16:11:18 +0900

    add animal

---
+++ ex00/main.cpp
@@ -0,0 +1,31 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main() {
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); // will output the cat sound!
+    j->makeSound();
+    meta->makeSound();
+
+    const WrongAnimal* wrong_meta = new WrongAnimal();
+    const WrongAnimal* wrong_cat = new WrongCat();
+
+    wrong_cat->makeSound(); // should output the WrongAnimal sound
+    wrong_meta->makeSound();
+
+    delete meta;
+    delete j;
+    delete i;
+    delete wrong_meta;
+    delete wrong_cat;
+
+    return 0;
+}
